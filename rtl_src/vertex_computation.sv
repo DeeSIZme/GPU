66c1196dd5d15e9df1d1691da3f414ffb9207917
Zamouth <guillaumezamouth@outlook.fr>
2022-11-30 16:54:38 +0100

    vertex_computation implemented

--- rtl_src/vertex_computation.sv
+++ rtl_src/vertex_computation.sv
@@ -7,9 +7,31 @@ module vertex_computation #(
   input rst,
   input start,
   input [COORD_WIDTH-1:0]vertexes[3][3],
-  output [COORD_WIDTH-1:0]vertexes_proj[3][2],
-  output [COORD_WIDTH-1:0]normal_vectors[3][2],
+  output [COORD_WIDTH-1:0]bounds[3][3],
   output eoc
 );
+
+wire [COORD_WIDTH-1:0]bounds[3][2];
+assign eoc = '1;
+
+always_comb begin
+  for (int i=0; i<MAX; ++i) begin
+        int j = (i+1)%3; // next vertex
+        next_bounds[i][0] = vertexes[i][1] - vertexes[j][1] ;
+        next_bounds[i][1] = vertexes[j][0] - vertexes[i][0] ;
+  end
+end
+
+always_ff @(posedge clk or negedge reset_n) begin
+  if(!reset_n) begin
+  end
+  else begin
+    for (int i=0; i<MAX; ++i) begin
+          bounds[i][0] <= next_bounds[i][0];
+          bounds[i][1] <= next_bounds[i][1];
+          bounds[i][2] <= -(next_bounds[i][0] * vertexes[i][0] + next_bounds[i][1] * vertexes[i][1]);
+    end
+  end
+end
 
 endmodule
